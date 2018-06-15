45a96cbb61648b8b04512553451ba7211f6a53fd
Luís Fonseca <fnsc.luis@gmail.com>
2018-06-14 23:09:56 -0300

    atualização banco de dados

--- formularios/combobox/consultar.php
+++ formularios/combobox/consultar.php
@@ -22,8 +22,6 @@
 define('DS', DIRECTORY_SEPARATOR);
 require_once __DIR__ .DS. '..' .DS. 'bootstrap.php';
 
-//use \PDO;
-
 // instancia o pdo
 $pdo = new PDO("{$config['driver']}:host={$config['host']};dbname={$config['dbname']};charset={$config['charset']}", $config['user'], $config['pass']);
 
